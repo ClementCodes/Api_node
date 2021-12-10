4337e39f66779df22b5b40dd3967a9959512a0da
clementcode <mrdominiqueclement@gmail.com>
2021-12-10 18:11:57 +0100

    cours soir vendredi

--- routes/students.route.js
+++ routes/students.route.js
@@ -1,33 +1,102 @@
 import express from "express";
-
+import faker from "faker";
 export const router = express.Router();
 
+
+
 router.get("/", (req, res) => {
-    res.send("Get all student")
+    const students = [
+        {
+            id: faker.datatype.uuid(),
+            name: faker.name.firstName(),
+            cursus: faker.name.jobType(),
+            average:faker.datatype.number({min:10,max:20})
+        }
+    
+    ]
+    res.status(200).json({message:"Get all acces ",students })
 });
+
+const studentByFive = () => {
+    let hello = []
+    const students = [
+        {
+            id: faker.datatype.uuid(),
+            name: faker.name.firstName(),
+            cursus: faker.name.jobType(),
+            average: faker.datatype.number({ min: 10, max: 20 })
+        }, {
+            id: faker.datatype.uuid(),
+            name: faker.name.firstName(),
+            cursus: faker.name.jobType(),
+            average: faker.datatype.number({ min: 10, max: 20 })
+        }, {
+            id: faker.datatype.uuid(),
+            name: faker.name.firstName(),
+            cursus: faker.name.jobType(),
+            average: faker.datatype.number({ min: 10, max: 20 })
+        }, {
+            id: faker.datatype.uuid(),
+            name: faker.name.firstName(),
+            cursus: faker.name.jobType(),
+            average: faker.datatype.number({ min: 10, max: 20 })
+        }, {
+            id: faker.datatype.uuid(),
+            name: faker.name.firstName(),
+            cursus: faker.name.jobType(),
+            average: faker.datatype.number({ min: 10, max: 20 })
+        }
+    ]
+return students.map(stud =>stud.push(hello))
+}
+
+
+
+const foisCinq = () => {
+    for (let index = 0; index < 5; index++) {
+        const students = [
+            {
+                id: faker.datatype.uuid(),
+                name: faker.name.firstName(),
+                cursus: faker.name.jobType(),
+                average:faker.datatype.number({min:10,max:20})
+            }
+        
+        ]
+        
+    }
+
+}
+
+
+console.log(studentByFive);
+
 
 router.get("/:id", (req, res) => {
 
     const studentId = req.params.id
     res.send(`Get id student ${studentId}`)
 });
-
-
 router.post('/', function (req, res) {
     console.log(req.body);
     const { id, name, location } = req.body
-    Object.keys(req.body).map(key => console.log(key, req.body[key]))
-    console.log(req.body.location);
-    res.send('Got a POST POST POST create a new student /students ');
+  const clef =   Object.keys(req.body).map(key => console.log(key, req.body[key]))
+    const rty = Object.values(req.body).filter(value => value.length <= 4)
+   
+  console.log(rty);
+    res.send(rty + clef);
   });
 
 router.put('/:id', function (req, res) {
-    res.send('Got a PUT request at /students/:id');
+    const { name} = req.body
+    console.log( name);
+    res.send(`hello ${name}`);
   });
 
 
 router.delete('/:id', function (req, res) {
-    res.send('Got a DELETE request at /user');
+    const { id } = req.body
+    res.send(`delete ${id}`);
 });
 
   
